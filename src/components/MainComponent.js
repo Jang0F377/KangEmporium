f0de00c9dffff66619f45328fb8eecfbcfffe86a
Matt Garrett <mjgarrett7092@gmail.com>
2022-07-27 19:08:39 -0700

    Started adding grids, responsiveness, and updated ui

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,31 +1,29 @@
 import HeaderComponent from "./HeaderComponent";
 import HomeComponent from "./HomeComponent";
-import {Route, Routes,Navigate} from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import ProductsComponent from "./ProductsComponent";
 import LoginScreen from "../screens/LoginScreen";
 import RegisterScreen from "../screens/RegisterScreen";
 import DetailedProductScreen from "../screens/DetailedProductScreen";
 import ViewCartScreen from "../screens/ViewCartScreen";
 import CheckoutScreen from "../screens/CheckoutScreen";
-import LoadingScreen from "../screens/LoadingScreen";
+import ViewCartScreenTW from "../screens/ViewCartScreenTW";
 
 const MainComponent = () => {
-
-
-    return (
-        <div className=''>
-            <HeaderComponent/>
-            <Routes>
-                <Route path='/' element={<HomeComponent/>}/>
-                <Route path='product' element={<ProductsComponent/>}/>
-                <Route path='login' element={<LoginScreen/>}/>
-                <Route path='register' element={<RegisterScreen/>}/>
-                <Route path='product/:name' element={<DetailedProductScreen/>}/>
-                <Route path='cart' element={<ViewCartScreen/>}/>
-                <Route path='checkout' element={<CheckoutScreen/>}/>
-            </Routes>
-        </div>
-    );
+  return (
+    <div className="bg-background-primary bg-main">
+      <HeaderComponent />
+      <Routes>
+        <Route path="/" element={<HomeComponent />} />
+        <Route path="product" element={<ProductsComponent />} />
+        <Route path="login" element={<LoginScreen />} />
+        <Route path="register" element={<RegisterScreen />} />
+        <Route path="product/:name" element={<DetailedProductScreen />} />
+        <Route path="cart" element={<ViewCartScreenTW />} />
+        <Route path="checkout" element={<CheckoutScreen />} />
+      </Routes>
+    </div>
+  );
 };
 
 export default MainComponent;
--- src/screens/RegisterScreen.js
+++ src/screens/RegisterScreen.js
@@ -1,94 +1,132 @@
-import {createUserWithEmailAndPassword} from 'firebase/auth';
-import {useState} from "react";
-import {Button, Input} from "@mui/material";
-import {useNavigate} from "react-router-dom";
-import {auth, db} from "../firebase";
-import {setDoc,doc,serverTimestamp} from 'firebase/firestore'
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { useState } from "react";
+import { Button, Input } from "@mui/material";
+import { useNavigate } from "react-router-dom";
+import { auth, db } from "../firebase";
+import { setDoc, doc, serverTimestamp } from "firebase/firestore";
 import LoadingScreen from "./LoadingScreen";
 import LoginSuccessComponent from "../components/LoginSuccessComponent";
 
 const RegisterScreen = () => {
-    const [firstName,setFirstName] = useState('');
-    const [email,setEmail] = useState('');
-    const [password,setPassword] = useState('');
-    const [confirmPassword,setConfirmPassword] = useState('');
-    const [loading,setLoading] = useState(false);
-    const [success,setSuccess] = useState(false);
-    const navigate = useNavigate();
-    const wait = (timeout) => {
-        return new Promise(resolve => setTimeout(resolve, timeout));
+  const [firstName, setFirstName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [success, setSuccess] = useState(false);
+  const navigate = useNavigate();
+  const wait = (timeout) => {
+    return new Promise((resolve) => setTimeout(resolve, timeout));
+  };
+  const handleKeypress = (e) => {
+    if (e.keyCode === 13) {
+      registrationFlow().then();
     }
-    const handleKeypress = (e) => {
-        if (e.keyCode === 13) {
-            registrationFlow().then();
-        }
-    };
+  };
 
-    const sendUserToFirestore = async (user,email) => {
-        await setDoc(doc(db,'users', `${email}`), {
-            firstName:firstName,
-            timestamp: serverTimestamp(),
-        })
-    }
+  const sendUserToFirestore = async (user, email) => {
+    await setDoc(doc(db, "users", `${email}`), {
+      firstName: firstName,
+      timestamp: serverTimestamp(),
+    });
+  };
 
-
-    const registrationFlow = async () => {
-        if (!loading) {
-            setSuccess(false);
-            setLoading(true);
-            await createUserWithEmailAndPassword(auth,email,password)
-                .then((userCred) => {
-                    const user = userCred.user;
-                    sendUserToFirestore(user,user.email).then(() => {
-                        wait(600).then(() => {
-                            setSuccess(true);
-                            setLoading(false);
-                        }).then(() => wait(3000).then(() => {
-                            setLoading(false);
-                            setSuccess(false);
-                        })).then(() => navigate('/',{replace:true}));
-                    }).catch(err => alert(err))
+  const registrationFlow = async () => {
+    if (!loading) {
+      setLoading(true);
+      setSuccess(false);
+      await createUserWithEmailAndPassword(auth, email, password)
+        .then((userCred) => {
+          const user = userCred.user;
+          sendUserToFirestore(user, user.email)
+            .then(() => {
+              wait(600)
+                .then(() => {
+                  setSuccess(true);
+                  setLoading(false);
                 })
-                .catch(err => alert(err))
-        }
-    };
+                .then(() =>
+                  wait(3000).then(() => {
+                    setLoading(false);
+                    setSuccess(false);
+                  })
+                )
+                .then(() => navigate("/", { replace: true }));
+            })
+            .catch((err) => alert(err));
+        })
+        .catch((err) => alert(err));
+    }
+  };
 
-    return(
-        <div className='flex lg:pb-11 flex-col bg-gray-200 mx-96 rounded items-center  pt-4'>
-            {loading ?
-                <LoadingScreen/> : success ?
-                    <LoginSuccessComponent/> :
-                    <div className='flex flex-col rounded items-center  pt-4'>
-                        <div className='text-7xl pt-2 text-center'>Welcome!</div>
-                        <div className='text-5xl pt-2 text-center'>Just a few deets</div>
-                        <div className='text-5xl pt-2 text-center'>is all we need:</div>
-                        <div className='p-1.5 m-5 bg-black w-96'/>
-                        <div className='flex flex-col'>
-                            <Input className='flex-1 mt-9 bg-white lg:w-72 rounded p-2' type='text' value={firstName}
-                                   onChange={(event) => setFirstName(event.target.value)}
-                                   placeholder={'First Name'}/>
-                            <Input className='flex-1 mt-9 bg-white lg:w-72 rounded p-2' type='email' value={email}
-                                   onChange={(event) => setEmail(event.target.value)}
-                                   placeholder={'Email'}/>
-                            <Input className='flex-1 mt-9 bg-white lg:w-72 rounded p-2' type='password' value={password}
-                                   onChange={(event) => setPassword(event.target.value)}
-                                   placeholder={'Password'}/>
-                            <Input className='flex-1 mt-9 bg-white lg:w-72 rounded p-2' type='password' value={confirmPassword}
-                                   onChange={(event) => {
-                                       setConfirmPassword(event.target.value)
-                                   }}
-                                   placeholder={'Confirm Password'}
-                                   onKeyDown={handleKeypress}
-                            />
-                            <Button disabled={!email || !firstName || !password || password !== confirmPassword} onClick={registrationFlow} sx={{marginTop:6}} variant={"contained"}>Register</Button>
-                            <div onClick={() => navigate('/login')} className='text-sm text-center mt-4 hover:cursor-pointer hover:text-blue-600'>Already have an account?</div>
-
-                        </div>
-                    </div>
-
-            }
+  return (
+    <div className="flex lg:pb-11 flex-col bg-gray-200 mx-96 rounded items-center  pt-4">
+      {loading ? (
+        <LoadingScreen />
+      ) : success ? (
+        <LoginSuccessComponent />
+      ) : (
+        <div className="flex flex-col rounded items-center  pt-4">
+          <div className="text-7xl pt-2 text-center">Welcome!</div>
+          <div className="text-5xl pt-2 text-center">Just a few deets</div>
+          <div className="text-5xl pt-2 text-center">is all we need:</div>
+          <div className="p-1.5 m-5 bg-black w-96" />
+          <div className="flex flex-col">
+            <Input
+              className="flex-1 mt-9 bg-white lg:w-72 rounded p-2"
+              type="text"
+              value={firstName}
+              onChange={(event) => setFirstName(event.target.value)}
+              placeholder={"First Name"}
+            />
+            <Input
+              className="flex-1 mt-9 bg-white lg:w-72 rounded p-2"
+              type="email"
+              value={email}
+              onChange={(event) => setEmail(event.target.value)}
+              placeholder={"Email"}
+            />
+            <Input
+              className="flex-1 mt-9 bg-white lg:w-72 rounded p-2"
+              type="password"
+              value={password}
+              onChange={(event) => setPassword(event.target.value)}
+              placeholder={"Password"}
+            />
+            <Input
+              className="flex-1 mt-9 bg-white lg:w-72 rounded p-2"
+              type="password"
+              value={confirmPassword}
+              onChange={(event) => {
+                setConfirmPassword(event.target.value);
+              }}
+              placeholder={"Confirm Password"}
+              onKeyDown={handleKeypress}
+            />
+            <Button
+              disabled={
+                !email ||
+                !firstName ||
+                !password ||
+                password !== confirmPassword
+              }
+              onClick={registrationFlow}
+              sx={{ marginTop: 6 }}
+              variant={"contained"}
+            >
+              Register
+            </Button>
+            <div
+              onClick={() => navigate("/login")}
+              className="text-sm text-center mt-4 hover:cursor-pointer hover:text-blue-600"
+            >
+              Already have an account?
+            </div>
+          </div>
         </div>
-    );
+      )}
+    </div>
+  );
 };
 
 export default RegisterScreen;
